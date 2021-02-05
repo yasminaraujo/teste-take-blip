5d96479a373066d426bb731fea5862ab76bdc05b
yasminaraujo <ygaraujo98@gmail.com>
2021-02-05 00:25:58 -0300

    Adiciona endpoint de get de repositorios

--- Controllers/TakeTestController.cs
+++ Controllers/TakeTestController.cs
@@ -2,10 +2,29 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using take_test.API.Services;
+using take_test.API.Domain.Model;
+using take_test.API.Domain.Services;
+
 namespace take_test.Controllers
 {
-    [Route("/api/[controller]")]
+    [Route("/api")]
+    [Produces("application/json")]
+    [ApiController]
     public class TakeTestController : Controller
     {
+        private readonly IGitHubAccessService _gitHubAccessService;
+
+        public TakeTestController (IGitHubAccessService gitHubAccessService){
+            _gitHubAccessService = gitHubAccessService;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(List<Repository>), 200)]
+        public async Task<List<Repository>> GetRepositories()
+        {
+            var repositories = await _gitHubAccessService.getGitHubRepositoriesData();
+            return repositories;
+        }
     }
 }
